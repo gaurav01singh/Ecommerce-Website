3016fb752d6d5348c8bb5c72cd72c43f1bfbc7a9
gaurav01singh <gauravsingh02195@gmail.com>
2025-04-01 21:43:31 +0530

    update for deployment

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ const __dirname = path.dirname(__filename);
 //middelwares
 app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build")));
+// app.use(morgan("dev"));
+// app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authroutes);
@@ -32,9 +32,9 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/bulid/index.html"));
-});
+// app.use("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./client/bulid/index.html"));
+// });
 
 //PORT
 const PORT = process.env.PORT || 8080;
