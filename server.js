ac49a5853de77709a5c0e92241c7f43921204245
gaurav01singh <gauravsingh02195@gmail.com>
2025-04-01 21:33:00 +0530

    update for deployment

--- server.js
+++ server.js
@@ -40,5 +40,5 @@ app.use("*", function (req, res) {
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}/`.bgCyan.white);
+  console.log(`http://localhost:${PORT}/`);
 });
